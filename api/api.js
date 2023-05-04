e92ee0e92f567a2898b9c0f2e30efd41041d6aa7
George Hill <georgeophill@outlook.com>
2023-05-04 12:33:06 +0100

    added middleware

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const logRoutes = require('./middleware/logger');
-const postRouter = require('./routers/post');
+const diaryRouter = require('./routers/diary');
 const userRouter = require('./routers/user');
 
 const api = express();
@@ -18,7 +18,7 @@ api.get("/", (req, res) => {
     })
 })
 
-api.use("/posts", postRouter);
+api.use("/diarys", diaryRouter);
 api.use("/users", userRouter);
 
 module.exports = api;
